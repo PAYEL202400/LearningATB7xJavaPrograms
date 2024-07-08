354e4364cba3aacdead0b8c528355ca11566474c
PAYEL202400 <payelbhadury2000@gmail.com>
2024-07-08 01:07:09 -0700

    new files are added now

---
+++ src/ex_07072024/lab012.java
@@ -0,0 +1,10 @@
+package ex_07072024;
+
+public class lab012 {
+    public static void main(String[] a) {
+        System.out.println("Interview Questions");
+
+        //is the above a valid code?
+        //yes or no?
+    }
+}
---
+++ src/ex_07072024/lab013.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class lab013 {
+    public static void main(String[] args) {
+
+     //can write static public void also
+    //can I run a class without the main method?-> no
+    }
+    //public static void main()
+        //this is not the entry point
+}
